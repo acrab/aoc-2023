09280f66501d1dd123cd919a15442ca546182d74
acrab <acrab@live.co.uk>
2023-12-11 13:21:30 +0000

    Tidy up day 7

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,65 +2,7 @@ import com.google.common.truth.Truth.assertThat
 
 fun main() {
 
-    data class Hand(val cardString: String, val bet: Long) {
-        val rank: Rank
-
-        init {
-            val cardTypes = cardString.associateBy { it }
-            rank = when (cardTypes.size) {
-                // One type of card means they're all the same (five of a kind)
-                1 -> Rank.FiveOfAKind
-                // Two types of card is either: ABBBB (4 of a kind) or AABBB (full house).
-                2 -> if (cardTypes.any { card -> cardString.count { it == card.key } == 4 }) Rank.FourOfAKind else Rank.FullHouse
-                // Three types of card is either: ABCCC (three of a kind) or ABBCC (two pair).
-                3 -> if (cardTypes.any { card -> cardString.count { it == card.key } == 3 }) Rank.ThreeOfAKind else Rank.TwoPair
-                // Four types of card must be ABCDD (one pair)
-                4 -> Rank.OnePair
-                // 5 types of card means all are different (high card)
-                5 -> Rank.HighCard
-                else -> error("Unexpected number of unique cards $cardTypes in $cardString")
-            }
-        }
-
-        val cardList: List<Int> = cardString.map {
-            if (it.isDigit()) {
-                it.digitToInt()
-            } else {
-                when (it) {
-                    'A' -> 14
-                    'K' -> 13
-                    'Q' -> 12
-                    'J' -> 11
-                    'T' -> 10
-                    else -> error("Unexpected rank $it")
-                }
-            }
-        }
-
-        fun compareTo(other: Hand): Int =
-            if (other.rank == this.rank) {
-                // compare cards
-                cardList.zip(other.cardList)
-                    .fold(0) { acc, pair -> if (acc == 0) pair.first.compareTo(pair.second) else acc }
-            } else {
-                // compare ranks
-                other.rank.compareTo(this.rank)
-            }
-
-
-    }
-
-    fun part1(input: List<String>): Long = input
-        .map {
-            val (cards, bet) = it.split(" ")
-            Hand(cards, bet.toLong())
-        }
-        .sortedWith(Hand::compareTo)
-        .mapIndexed { index, hand -> hand.bet * (index + 1) }
-        .sum()
-
-
-    data class HandWithJokers(val cardString: String, val bet: Long) {
+    data class HandWithJokers(val cardString: String, val bet: Long, val withJokers: Boolean) {
         var rank: Rank
             private set
 
@@ -69,13 +11,16 @@ fun main() {
             cardString.forEach { card ->
                 cardTypes[card] = cardString.count { it == card }
             }
-            val numJokers = cardTypes['J'] ?: 0
-            cardTypes.remove('J')
-            if (cardTypes.isEmpty()) {
-                cardTypes['J'] = numJokers
-            } else {
-                val maxCount = cardTypes.maxBy { it.value }
-                cardTypes[maxCount.key] = maxCount.value + numJokers
+
+            // If we're using jokers, turn them all into the card with the highest count
+            if (withJokers) {
+                val numJokers = cardTypes['J'] ?: 0
+                // Modify the hand unless it's all jokers
+                if (numJokers != 5) {
+                    cardTypes.remove('J')
+                    val maxCount = cardTypes.maxBy { it.value }
+                    cardTypes[maxCount.key] = maxCount.value + numJokers
+                }
             }
 
             rank = when (cardTypes.size) {
@@ -101,9 +46,9 @@ fun main() {
                     'A' -> 14
                     'K' -> 13
                     'Q' -> 12
+                    // Jokers are the least valuable card
+                    'J' -> if (withJokers) 1 else 11
                     'T' -> 10
-                    // 'J' is now a joker, and is the least valued card
-                    'J' -> 1
                     else -> error("Unexpected rank $it")
                 }
             }
@@ -118,35 +63,27 @@ fun main() {
                 // compare ranks
                 other.rank.compareTo(this.rank)
             }
-
-
     }
 
-    fun part2(input: List<String>) = input
+    fun scoreHands(input: List<String>, withJokers: Boolean): Long = input
         .map {
             val (cards, bet) = it.split(" ")
-            HandWithJokers(cards, bet.toLong())
+            HandWithJokers(cards, bet.toLong(), withJokers)
         }
         .sortedWith(HandWithJokers::compareTo)
-        .mapIndexed { index, hand ->
-            val strength = index + 1
-            val score = hand.bet * strength
-            println("Given ${hand.cardString} (${hand.rank}, $${hand.bet}) rank $strength, scoring $score")
-            score
-        }
+        .mapIndexed { index, hand -> hand.bet * (index + 1) }
         .sum()
 
     val testInput = readInput("Day07_test")
-    assertThat(part1(testInput)).isEqualTo(6440L)
+    assertThat(scoreHands(testInput, withJokers = false)).isEqualTo(6440L)
 
     println("-----")
     val input = readInput("Day07")
-    part1(input).println()
+    scoreHands(input, withJokers = false).println()
 
     println("-----")
-    assertThat(part2(testInput)).isEqualTo(5905)
-    println("-----")
-    part2(input).println()
+    assertThat(scoreHands(testInput, withJokers = true)).isEqualTo(5905)
+    scoreHands(input, withJokers = true).println()
 }
 
 private enum class Rank {
